b9b91033d30a23f12d2ab47cd718cbfc83390714
Saddam Khalid <19011519-029@uog.edu.pk>
2022-02-27 08:41:51 -0800

    Add files via upload

---
+++ Lec8PracticeSet/Form1.Designer.cs
@@ -0,0 +1,344 @@
+﻿namespace Lec8PracticeSet
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblId = new System.Windows.Forms.Label();
+            this.lblAge = new System.Windows.Forms.Label();
+            this.lblmail = new System.Windows.Forms.Label();
+            this.lblGender = new System.Windows.Forms.Label();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.txtMail = new System.Windows.Forms.TextBox();
+            this.lblDOB = new System.Windows.Forms.Label();
+            this.txtGender = new System.Windows.Forms.ComboBox();
+            this.txtDOB = new System.Windows.Forms.DateTimePicker();
+            this.btnPrev = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnNew = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.lbloutput = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuItemSave = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.menuStrip1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(298, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(153, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Registration Form";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.Location = new System.Drawing.Point(96, 109);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(116, 16);
+            this.lblName.TabIndex = 1;
+            this.lblName.Text = "Student Name : ";
+            // 
+            // lblId
+            // 
+            this.lblId.AutoSize = true;
+            this.lblId.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblId.Location = new System.Drawing.Point(96, 75);
+            this.lblId.Name = "lblId";
+            this.lblId.Size = new System.Drawing.Size(80, 16);
+            this.lblId.TabIndex = 2;
+            this.lblId.Text = "Student Id:";
+            // 
+            // lblAge
+            // 
+            this.lblAge.AutoSize = true;
+            this.lblAge.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAge.Location = new System.Drawing.Point(96, 148);
+            this.lblAge.Name = "lblAge";
+            this.lblAge.Size = new System.Drawing.Size(103, 16);
+            this.lblAge.TabIndex = 3;
+            this.lblAge.Text = "Student Age : ";
+            // 
+            // lblmail
+            // 
+            this.lblmail.AutoSize = true;
+            this.lblmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblmail.Location = new System.Drawing.Point(96, 184);
+            this.lblmail.Name = "lblmail";
+            this.lblmail.Size = new System.Drawing.Size(100, 16);
+            this.lblmail.TabIndex = 4;
+            this.lblmail.Text = "Student Mail: ";
+            // 
+            // lblGender
+            // 
+            this.lblGender.AutoSize = true;
+            this.lblGender.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGender.Location = new System.Drawing.Point(96, 218);
+            this.lblGender.Name = "lblGender";
+            this.lblGender.Size = new System.Drawing.Size(122, 16);
+            this.lblGender.TabIndex = 5;
+            this.lblGender.Text = "Student Gender: ";
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(241, 71);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(189, 20);
+            this.txtID.TabIndex = 6;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(241, 109);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(189, 20);
+            this.txtName.TabIndex = 7;
+            // 
+            // txtAge
+            // 
+            this.txtAge.Location = new System.Drawing.Point(241, 144);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(189, 20);
+            this.txtAge.TabIndex = 8;
+            // 
+            // txtMail
+            // 
+            this.txtMail.Location = new System.Drawing.Point(241, 180);
+            this.txtMail.Name = "txtMail";
+            this.txtMail.Size = new System.Drawing.Size(189, 20);
+            this.txtMail.TabIndex = 9;
+            // 
+            // lblDOB
+            // 
+            this.lblDOB.AutoSize = true;
+            this.lblDOB.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDOB.Location = new System.Drawing.Point(96, 255);
+            this.lblDOB.Name = "lblDOB";
+            this.lblDOB.Size = new System.Drawing.Size(103, 16);
+            this.lblDOB.TabIndex = 10;
+            this.lblDOB.Text = "Student DOB: ";
+            // 
+            // txtGender
+            // 
+            this.txtGender.FormattingEnabled = true;
+            this.txtGender.Items.AddRange(new object[] {
+            "Male",
+            "Female"});
+            this.txtGender.Location = new System.Drawing.Point(241, 218);
+            this.txtGender.Name = "txtGender";
+            this.txtGender.Size = new System.Drawing.Size(146, 21);
+            this.txtGender.TabIndex = 11;
+            // 
+            // txtDOB
+            // 
+            this.txtDOB.Location = new System.Drawing.Point(241, 255);
+            this.txtDOB.Name = "txtDOB";
+            this.txtDOB.Size = new System.Drawing.Size(200, 20);
+            this.txtDOB.TabIndex = 12;
+            // 
+            // btnPrev
+            // 
+            this.btnPrev.Location = new System.Drawing.Point(124, 310);
+            this.btnPrev.Name = "btnPrev";
+            this.btnPrev.Size = new System.Drawing.Size(75, 23);
+            this.btnPrev.TabIndex = 13;
+            this.btnPrev.Text = "<<";
+            this.btnPrev.UseVisualStyleBackColor = true;
+            this.btnPrev.Click += new System.EventHandler(this.btnPrev_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(302, 310);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 14;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnNew
+            // 
+            this.btnNew.Location = new System.Drawing.Point(211, 310);
+            this.btnNew.Name = "btnNew";
+            this.btnNew.Size = new System.Drawing.Size(75, 23);
+            this.btnNew.TabIndex = 15;
+            this.btnNew.Text = "New";
+            this.btnNew.UseVisualStyleBackColor = true;
+            this.btnNew.Click += new System.EventHandler(this.btnNew_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(396, 310);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(75, 23);
+            this.btnNext.TabIndex = 16;
+            this.btnNext.Text = ">>";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // lbloutput
+            // 
+            this.lbloutput.Location = new System.Drawing.Point(121, 371);
+            this.lbloutput.Name = "lbloutput";
+            this.lbloutput.Size = new System.Drawing.Size(205, 23);
+            this.lbloutput.TabIndex = 17;
+            this.lbloutput.Text = "Output";
+            this.lbloutput.UseVisualStyleBackColor = true;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 18;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
+            this.mnuItemSave});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.newToolStripMenuItem.Text = "New";
+            this.newToolStripMenuItem.Click += new System.EventHandler(this.newToolStripMenuItem_Click);
+            // 
+            // mnuItemSave
+            // 
+            this.mnuItemSave.Name = "mnuItemSave";
+            this.mnuItemSave.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.mnuItemSave.Size = new System.Drawing.Size(180, 22);
+            this.mnuItemSave.Text = "&Safe";
+            this.mnuItemSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 24);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(800, 25);
+            this.toolStrip1.TabIndex = 19;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton1.Text = "toolStripButton1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 457);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.lbloutput);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.btnNew);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnPrev);
+            this.Controls.Add(this.txtDOB);
+            this.Controls.Add(this.txtGender);
+            this.Controls.Add(this.lblDOB);
+            this.Controls.Add(this.txtMail);
+            this.Controls.Add(this.txtAge);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.lblGender);
+            this.Controls.Add(this.lblmail);
+            this.Controls.Add(this.lblAge);
+            this.Controls.Add(this.lblId);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Student Form";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblId;
+        private System.Windows.Forms.Label lblAge;
+        private System.Windows.Forms.Label lblmail;
+        private System.Windows.Forms.Label lblGender;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.TextBox txtMail;
+        private System.Windows.Forms.Label lblDOB;
+        private System.Windows.Forms.ComboBox txtGender;
+        private System.Windows.Forms.DateTimePicker txtDOB;
+        private System.Windows.Forms.Button btnPrev;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnNew;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button lbloutput;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mnuItemSave;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+    }
+}
+
---
+++ Lec8PracticeSet/Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+
+namespace Lec8PracticeSet
+{
+    public partial class Form1 : Form
+    {
+        List<Student> students = new List<Student>();   
+        Student currStudent=null;
+        int currIndex = -1;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        // At Step1.Form Load event will call
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if(File.Exists("temp.txt"))
+                ReadDataFromFile();
+
+            if (students.Count > 0)
+            { 
+                currIndex = students.Count - 1;
+                currStudent = students[currIndex];
+                
+            }
+            if(currStudent!=null)
+                FromDataToUI();
+            EnableControls();
+        }
+        private void btnNew_Click(object sender, EventArgs e)
+        {
+            currStudent = new Student();
+            currIndex = students.Count;
+            EnableControls();
+            FromDataToUI();
+        }
+
+        private void FromDataToUI()
+        {
+            txtID.Text = Convert.ToString(currStudent.ID);
+            txtName.Text=currStudent.Name;
+            txtAge.Text=Convert.ToString(currStudent.Age);
+            txtGender.SelectedIndex=currStudent.Gender==Student.GENDER.Male?0:1;
+            txtMail.Text = currStudent.Gmail;
+            txtDOB.Value = currStudent.DOB;
+            
+        }
+
+        private void EnableControls()
+        {
+            if (currStudent == null)
+            {
+                txtID.Enabled = false;
+                txtName.Enabled=false;
+                txtAge.Enabled = false;
+                txtMail.Enabled = false;
+                txtGender.Enabled = false;
+                txtDOB.Enabled=false;
+                btnSave.Enabled=false;
+                btnPrev.Enabled=false;
+                btnNext.Enabled=false;
+            }
+            else
+            {
+                txtID.Enabled =true;
+                txtName.Enabled =true;
+                txtAge.Enabled = true;
+                txtMail.Enabled = true;
+                txtGender.Enabled = true;
+                txtDOB.Enabled = true;
+                btnSave.Enabled = true;
+            }
+            if (currIndex > 0)
+            {
+                btnPrev.Enabled = true;
+            }
+            else
+            {
+                btnPrev.Enabled = false;
+
+            }
+            if (currIndex < students.Count - 1)
+            {
+                btnNext.Enabled = true;
+            }
+            else
+            {
+                btnNext.Enabled= false;
+            }
+            
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            FromUIToData();
+            if(currIndex == students.Count)
+            {
+                students.Add(currStudent);
+            }
+            lbloutput.Text = students.Count.ToString();
+
+            WriteDataToFile();
+        }
+
+        private void FromUIToData()
+        {
+            currStudent.ID=Convert.ToInt32(txtID.Text);
+            currStudent.Name=txtName.Text;
+            currStudent.Age= Convert.ToInt32(txtAge.Text);
+            currStudent.Gender = txtGender.SelectedIndex == 0 ? Student.GENDER.Male : Student.GENDER.female;
+            currStudent.Gmail= txtMail.Text;
+           // currStudent.selec
+        }
+
+        private void btnPrev_Click(object sender, EventArgs e)
+        {
+            if (currIndex <= 0) {
+                return;
+            }
+            currIndex--;
+            currStudent=students[currIndex];
+            FromDataToUI();
+            EnableControls();
+
+        }
+        public void ReadDataFromFile()
+        {
+
+
+            FileStream filestream = new FileStream("temp.txt", FileMode.OpenOrCreate);
+        TextReader textReader = new StreamReader(filestream);
+
+        XmlSerializer xmlSerializer = new XmlSerializer(typeof(List<Student>));
+        students = (List<Student>) xmlSerializer.Deserialize(textReader);
+
+
+        textReader.Close();
+        filestream.Close();
+    }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            if (currIndex >= students.Count-1) {
+
+                return;
+            }
+            currIndex++;
+            currStudent= students[currIndex];
+            FromDataToUI();
+            EnableControls();
+        }
+        public void WriteDataToFile()
+        {
+            FileStream filestream = new FileStream("temp.txt", FileMode.OpenOrCreate);
+            TextWriter textWriter = new StreamWriter(filestream);
+
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(List<Student>));
+            xmlSerializer.Serialize(textWriter, students);
+
+
+            textWriter.Close();
+            filestream.Close();
+        }
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            btnNew_Click(sender, e);
+        }
+    }
+   
+}
+
